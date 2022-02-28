ef22a05506c41e7cd1ff19dabf91e4305c3342c4
ngovanthai123 <ngothai781@gmail.com>
2022-02-28 16:02:35 +0700

    tao gio hang

---
+++ CuaHangBanLapTop/CuaHangBanLapTop/Controllers/cart.cs
@@ -0,0 +1,37 @@
+﻿using CuaHangBanLapTop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuaHangBanLapTop.Controllers
+{
+    public class cart : Controller
+    {
+        private readonly ILogger<cart> _logger;
+
+        public cart(ILogger<cart> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CuaHangBanLapTop/CuaHangBanLapTop/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuaHangBanLapTop.Models
+{
+    public class CartItem
+    {
+        public Sanpham Sanpham { get; set; }
+        public Phukien Phukien { get; set; }
+        public int Quantity { get; set; }
+    }
+}
